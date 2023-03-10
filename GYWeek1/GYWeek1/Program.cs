f1dd167f1599b024b56e08cab43de73c3a06e707
Mehmet Kılınç <mehmetkilinc2734@gmail.com>
2023-03-10 23:28:20 +0300

    The min-max element, sum and average of an number array

--- GYWeek1/GYWeek1/Program.cs
+++ GYWeek1/GYWeek1/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int[] numbers = { 1, 5, 90, 13, 63, 78, 4, 9, 16 };
+
+int total = 0;
+int max = numbers[0];
+int min = numbers[0];
+
+for (int i = 1; i < numbers.Length; i++)
+{
+    //En büyük sayı
+    if (numbers[i] > max)
+        max = numbers[i];
+}
+
+for (int i = 1; i < numbers.Length; i++)
+{
+    //En küçük sayı 
+    if (numbers[i] < min)
+        min = numbers[i];
+}
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    //Dizi elemanları toplamı
+    total += numbers[i];
+}
+
+float average = (float)total / numbers.Length;
+
+Console.WriteLine($"Dizinin en büyük elemanı : {max} \nDizinin en küçük elemanı : {min}\n" +
+                  $"Dizi elemanlarının toplamı : {total}\nDizi elemanlarının ortalaması : {average}");
